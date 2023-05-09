07b0dc1f73556977cf3375b3a19202be813948f1
VictorRoe <victorroe819@gmail.com>
2023-05-09 10:55:42 -0500

    feat: Add swagger APIs documentation

--- Back-End/palmbooking/src/main/java/com/backend/palmbooking/PalmbookingApplication.java
+++ Back-End/palmbooking/src/main/java/com/backend/palmbooking/PalmbookingApplication.java
@@ -3,11 +3,13 @@ package com.backend.palmbooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PalmbookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PalmbookingApplication.class, args);
+
 	}
 
 }
