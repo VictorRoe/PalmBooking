0b6c8159b7e082a70584fc34c57a89705d1cd9b9
VictorRoe <victorroe819@gmail.com>
2023-05-09 15:32:08 -0500

    feat: Add CRUD image & Add annotation in ProductRepository

---
+++ Back-End/palmbooking/src/main/java/com/backend/palmbooking/Controller/ImageController.java
@@ -0,0 +1,62 @@
+package com.backend.palmbooking.Controller;
+
+import com.backend.palmbooking.Model.Image;
+import com.backend.palmbooking.Model.Product;
+import com.backend.palmbooking.Service.ImageService;
+import com.backend.palmbooking.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/image")
+public class ImageController {
+
+    @Autowired
+    private final ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @GetMapping
+    public List<Image> getImage() {
+        return imageService.getImage();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Image> getImageByID(@PathVariable Long id){
+        return imageService.getImageByID(id);
+    }
+
+    @PostMapping
+    public void addImage(@RequestBody Image image){
+        imageService.addImage(image);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> editImage(@RequestBody Image image) {
+        Optional<Image> searchImage = imageService.getImageByID(image.getId());
+
+        if (searchImage.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        imageService.editImage(image);
+        return ResponseEntity.status(202).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteImageByID(@PathVariable Long id) {
+        Optional<Image> searchImage = imageService.getImageByID(id);
+
+        if (searchImage.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        imageService.deleteImageByID(id);
+        return ResponseEntity.status(204).build();
+    }
+}
--- Back-End/palmbooking/src/main/java/com/backend/palmbooking/Repository/ImageRepository.java
+++ Back-End/palmbooking/src/main/java/com/backend/palmbooking/Repository/ImageRepository.java
@@ -1,9 +1,9 @@
 package com.backend.palmbooking.Repository;
 
-import com.backend.palmbooking.Model.Product;
+import com.backend.palmbooking.Model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ImageRepository extends JpaRepository<Image, Long> {
 }
