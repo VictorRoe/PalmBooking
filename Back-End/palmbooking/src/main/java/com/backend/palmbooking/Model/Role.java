c196539d9200db18df0cf3f54879c0c26acbf288
VictorRoe <victorroe819@gmail.com>
2023-06-08 16:52:01 -0500

    feat: add UserDetails Implement (Spring Security)

---
+++ Back-End/palmbooking/src/main/java/com/backend/palmbooking/Model/Role.java
@@ -0,0 +1,7 @@
+package com.backend.palmbooking.Model;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
--- Back-End/palmbooking/src/main/java/com/backend/palmbooking/Model/User.java
+++ Back-End/palmbooking/src/main/java/com/backend/palmbooking/Model/User.java
@@ -3,27 +3,72 @@ package com.backend.palmbooking.Model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table (name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name",length = 20)
+    @Column(name = "name",length = 20, nullable = false)
     private String name;
 
-    @Column(name = "last_name", length = 20)
+    @Column(name = "last_name", length = 20, nullable = false)
     private String lastName;
 
-    @Column(name = "email", unique = true)
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
